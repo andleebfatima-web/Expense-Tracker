36cfd1615e143edd9c759b84d513d391fbb35637
AndleebF <andleeb.fatime173@gmail.com>
2026-09-17 07:10:27 +0500

    fix: fix the ts issue for expenxe

--- server/src/utilityfunc.ts
+++ server/src/utilityfunc.ts
@@ -1,10 +1,8 @@
 import type { Expense } from "./types.js";
 export function calculateTotal(expenses: Expense[]): number {
- let sum:number=0;
-     for(let i=0;i<expenses.length;i++){
-        console.log(expenses[i]);
-        sum+=expenses[i].amount;
-     }
- console.log(expenses)
- return sum;
+  let sum = 0;
+  for (const expense of expenses) {
+    sum += expense.amount;
+  }
+  return sum;
 }
